89e8ab9bd506c0cbe9e88d80e78fc0e94ae8cf98
Sagarsunil16 <155360655+Sagarsunil16@users.noreply.github.com>
2025-04-08 17:45:36 +0530

    Project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,6 +22,9 @@ app.use('/api/messages',messageRoute)
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    app.get('/*',(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
 }
 server.listen(PORT,()=>{
     console.log(`Server is listening on ${process.env.PORT}`)
