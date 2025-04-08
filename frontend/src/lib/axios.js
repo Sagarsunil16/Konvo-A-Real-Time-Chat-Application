19632c1e458ed5e408b5abd2d0fba55c3175c38c
Sagarsunil16 <155360655+Sagarsunil16@users.noreply.github.com>
2025-04-08 17:56:27 +0530

    all cleared

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL:import.meta.env.MODE === "developement"?' http://localhost:3000/api' : "/",
+    baseURL:import.meta.env.MODE === "developement"?' http://localhost:3000/api' : "/api",
     withCredentials:true
 })
